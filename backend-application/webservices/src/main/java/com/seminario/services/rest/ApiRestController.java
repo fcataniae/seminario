aa8f4dafcf89332b06d61d41a01feb0437a75d23
fcataniae <cataniafrane@gmail.com>
2019-04-03 23:44:09 -0300

    dependencias borradas

--- backend-application/webservices/src/main/java/com/seminario/services/rest/ApiRestController.java
+++ backend-application/webservices/src/main/java/com/seminario/services/rest/ApiRestController.java
@@ -7,25 +7,13 @@ import com.seminario.backend.model.Permiso;
 import com.seminario.backend.model.Persona;
 import com.seminario.backend.model.Rol;
 import com.seminario.backend.model.Usuario;
-import org.springframework.boot.json.GsonJsonParser;
 import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.*;
 import com.seminario.backend.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.util.UriComponentsBuilder;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import com.google.gson.Gson;
-
-import javax.json.Json;
 import java.util.List;
 
 /**
