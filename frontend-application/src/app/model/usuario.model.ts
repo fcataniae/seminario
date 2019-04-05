66ece338798be00c0ab2d88bf1a38d3bc1749048
fcataniae <cataniafrane@gmail.com>
2019-04-05 00:33:33 -0300

    cambios en front

--- frontend-application/src/app/model/usuario.model.ts
+++ frontend-application/src/app/model/usuario.model.ts
@@ -1,9 +1,11 @@
 import { Persona } from './persona.model';
+import { Estado } from './estado.model';
 
 export class Usuario {
 
   nombreUsuario: string;
   password: string;
   persona: Persona;
+  estado: Estado;
 
 }
