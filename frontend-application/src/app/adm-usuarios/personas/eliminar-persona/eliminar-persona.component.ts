e1fea1cc06ffe6ac39db93ae6b6e089c1eac7210
fcataniae <cataniafrane@gmail.com>
2019-04-05 18:21:04 -0300

    Modificar y eliminar persona con rutas y pegandole al back

--- frontend-application/src/app/adm-usuarios/personas/eliminar-persona/eliminar-persona.component.ts
+++ frontend-application/src/app/adm-usuarios/personas/eliminar-persona/eliminar-persona.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PersonaService } from './../../../services/persona.service';
 import { Persona } from './../../../model/persona.model';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,7 +12,7 @@ import { Persona } from './../../../model/persona.model';
 export class EliminarPersonaComponent implements OnInit {
 
   constructor(private _personaService: PersonaService,
-              private _router: RouterService) { }
+              private _router: Router) { }
 
   documentoBusqueda: number;
   persona: Persona;
