b51f3b386c6bdc55c49880b9529f78186a0f1635
mrgrassho <mr.grassho@gmail.com>
2019-04-10 22:13:30 -0300

    Elimina Archivos viejos.

--- backend-application/backend/src/main/java/com/seminario/backend/services/interfaces/IEstadoService.java
+++
@@ -1,7 +0,0 @@
-package com.seminario.backend.services.interfaces;
-
-import com.seminario.backend.model.Estado;
-
-public interface IEstadoService {
-    Estado getEstadoByNombre(String nombre);
-}
