0cbca55af01f98582a3af7871c8cf86fa726f077
Mariano Zabala <marianol.zabala@gmail.com>
2019-05-21 18:59:46 -0300

    Envio frontEnd (html, css, ts)

--- frontend-application/src/app/bienes/envio/envio.component.ts
+++ frontend-application/src/app/bienes/envio/envio.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
+
+export class Bien{
+  tipoBien: string;
+  bien: string;
+  tipoDoc: string;
+  nroDoc: number;
+  cantidad: number;
+  vacio: boolean;
+}
+
+export class Recurso{
+  tipoRecurso: string;
+  idRecurso: number;
+}
 
 @Component({
   selector: 'app-envio',
@@ -7,9 +22,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnvioComponent implements OnInit {
 
+  @ViewChild(MatSort) sortBienes: MatSort;
+  @ViewChild(MatPaginator) paginatorBienes: MatPaginator;
+  datosTablaBienes = new MatTableDataSource<Bien>();
+
+  @ViewChild(MatSort) sortRecursos: MatSort;
+  @ViewChild(MatPaginator) paginatorRecursos: MatPaginator;
+  datosTablaRecursos = new MatTableDataSource<Recurso>();
+
+  columnsToDisplayBien = ['tipoBien','bien','tipoDoc','nroDoc','cantidad','vacio'];
+  columnsToDisplayRecurso = ['tipoRecurso','idRecurso'];
+
   constructor() { }
 
   ngOnInit() {
+
+  let bienesAgregados: Bien[] = [{tipoBien: 'Pallet', bien: 'ARLOG', tipoDoc: 'Recibo', nroDoc: 0, cantidad: 5, vacio: false}];
+
+  this.datosTablaBienes.data = bienesAgregados;
+  this.datosTablaBienes.sort = this.sortBienes;
+  this.datosTablaBienes.paginator = this.paginatorBienes;
+
+  let recursosAgregados: Recurso[] = [{tipoRecurso: 'Termógrafo', idRecurso: 0}];
+
+  this.datosTablaRecursos.data = recursosAgregados;
+  this.datosTablaRecursos.sort = this.sortRecursos;
+  this.datosTablaRecursos.paginator = this.paginatorRecursos;
+
   }
 
 }
