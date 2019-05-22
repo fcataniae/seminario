adb8ef372a0f06ad10f6959ed768656bab299326
Matias <matirolon96@gmail.com>
2019-05-22 18:24:37 -0300

    Listar estados y agregados en db/csv

--- backend-application/backend/src/main/java/com/seminario/backend/services/abm/EstadoService.java
+++ backend-application/backend/src/main/java/com/seminario/backend/services/abm/EstadoService.java
@@ -1,9 +1,14 @@
 package com.seminario.backend.services.abm;
 
+
 import com.seminario.backend.model.abm.Estado;
+import com.seminario.backend.model.abm.Usuario;
 import com.seminario.backend.repository.abm.EstadoRepository;
+import com.seminario.backend.repository.abm.PermisoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 
 @Service
@@ -12,9 +17,19 @@ public class EstadoService {
     @Autowired
     private EstadoRepository estadoRepository;
 
+
+    @Autowired
+    PermisoRepository permisoRepository;
+
     public Estado getEstadoByNombre(String nombre){
         return estadoRepository.findByDescrip(nombre);
     }
 
-
+    public List<Estado> getAllEstados(Usuario usuarioActual){
+        if (null != permisoRepository.findPermisoWhereUsuarioAndPermiso(usuarioActual.getId(),"CONS-ESTADO")) {
+            return estadoRepository.findAll();
+        } else {
+            throw new CustomException("No cuenta con los permisos para consultar los estados");
+        }
+    }
 }
