e3b956296c22af66e4a06c8a4b8f1da932658e6b
fcataniae <cataniafrane@gmail.com>
2019-05-23 22:41:24 -0300

    model movimiento

--- frontend-application/src/app/model/bienes/movimiento.model.ts
+++ frontend-application/src/app/model/bienes/movimiento.model.ts
@@ -1,10 +1,16 @@
 import { TipoMovimiento } from "./tipomovimiento.model";
 import { Recurso } from "./recurso.model";
+import { ItemMovimiento } from "./itemmovimiento.model";
 
 export class Movimiento{
   tipoMovimiento : TipoMovimiento;
   origen: number;
   destino: number;
   recursosAsignados: Recurso[];
-  
+  items : ItemMovimiento[];
+  constructor(){
+    this.recursosAsignados = [];
+    this.items = []
+  }
+
 }
