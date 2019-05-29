84ee2775ef6de4d4517a91807378aaf0c8f50eea
fcataniae <cataniafrane@gmail.com>
2019-05-28 22:53:40 -0300

    second datasource example

--- backend-application/backend/src/main/java/com/seminario/backend/JNDIAdapterConfigDev.java
+++ backend-application/backend/src/main/java/com/seminario/backend/JNDIAdapterConfigDev.java
@@ -1,14 +1,17 @@
 package com.seminario.backend;
 
 import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
@@ -25,6 +28,22 @@ public class JNDIAdapterConfigDev {
     @ConfigurationProperties(prefix = "seminario.datasource")
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "walmartdb")
+    @ConfigurationProperties(prefix="secondarydatabase.datasource")
+    public DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "waltmartem")
+    public LocalContainerEntityManagerFactoryBean storingEntityManagerFactory(
+            EntityManagerFactoryBuilder builder, @Qualifier("walmartdb") DataSource ds) {
+        return builder
+                .dataSource(ds)
+                .packages("com.seminario.backend.dto")
+                .persistenceUnit("waltmartempu")
+                .build();
     }
 
     @Bean
