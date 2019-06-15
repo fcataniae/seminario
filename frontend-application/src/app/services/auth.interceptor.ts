b2620f14141f6bea81d0dae65894dc4b42b18e95
fcataniae <cataniafrane@gmail.com>
2019-06-15 00:33:03 -0300

    cambios para catchear errores 403 y para que la validacion de usuario muestre error por contraseña/usuario incorrecto

--- frontend-application/src/app/services/auth.interceptor.ts
+++ frontend-application/src/app/services/auth.interceptor.ts
@@ -8,15 +8,16 @@ import {
 } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { catchError } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 import { MatDialog } from '@angular/material';
 import { ConfirmacionPopupComponent } from '../adm-usuarios/confirmacion-popup/confirmacion-popup.component';
+import { Router } from '@angular/router';
 
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private _matdialog: MatDialog){}
+  constructor(private _matdialog: MatDialog, private _router : Router){}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -35,6 +36,16 @@ export class AuthInterceptor implements HttpInterceptor {
               data: {mensaje:"Ocurrio un error inesperado: " + err.error.message, titulo: "Error:",error: true}
             });
             dialog.afterClosed().subscribe();
+          }else if (err.status === 403){
+            console.log(window.location.href);
+            if(!window.location.href.includes('login')){
+              let dialog = this._matdialog.open(ConfirmacionPopupComponent,{
+                data: {mensaje:"No se pudo autenticar al usuario!",error: true}
+              });
+              dialog.afterClosed().subscribe( res => this._router.navigate(['/login']));
+            }else{
+              return throwError("Error al autenticar el usuario");
+            }
           }
         }
         return of(err);
