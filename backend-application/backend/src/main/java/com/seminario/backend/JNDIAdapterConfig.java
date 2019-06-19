c25d1a984ac5a4672ed7bf4cabda2def254bd557
fcataniae <cataniafrane@gmail.com>
2019-06-19 19:44:30 -0300

    agrega configuracion productiva y cambia el scope de libreria maria db

--- backend-application/backend/src/main/java/com/seminario/backend/JNDIAdapterConfig.java
+++ backend-application/backend/src/main/java/com/seminario/backend/JNDIAdapterConfig.java
@@ -1,15 +1,19 @@
 package com.seminario.backend;
 
 import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
@@ -21,14 +25,33 @@ import javax.sql.DataSource;
 public class JNDIAdapterConfig {
 
     @Primary
-    @Bean(name = "dsSeminario",destroyMethod = "")
+    @Bean(name = "dsSeminario")
     public DataSource dataSource() throws NamingException {
-        InitialContext context = new InitialContext();
-        return (DataSource) context.lookup(precalificador().getJndiName());
+        return getDataSourceByName(seminario().getJndiName());
+    }
+    @Bean(name = "walmartdb")
+    public DataSource secondaryDataSource() {
+
+        return getDataSourceByName(walmart().getJndiName());
+    }
+    @Bean(name = "waltmartem")
+    public LocalContainerEntityManagerFactoryBean storingEntityManagerFactory(
+            EntityManagerFactoryBuilder builder, @Qualifier("walmartdb") DataSource ds) {
+        return builder
+                .dataSource(ds)
+                .packages("com.seminario.backend.dto")
+                .persistenceUnit("waltmartempu")
+                .build();
     }
 
+
+
     @Bean@ConfigurationProperties(prefix = "seminario.datasource")
-    public JndiPropertyHolder precalificador() {
+    public JndiPropertyHolder seminario() {
+        return new JndiPropertyHolder();
+    }
+    @Bean@ConfigurationProperties(prefix = "secondarydatabase.datasource")
+    public JndiPropertyHolder walmart() {
         return new JndiPropertyHolder();
     }
 
@@ -51,4 +74,17 @@ public class JNDIAdapterConfig {
         return liquibase;
     }
 
+    public DataSource getDataSourceByName(String name){
+
+        Context c;
+        try
+        {
+            c = new InitialContext();
+            return (DataSource) c.lookup("java:comp/env/" + name);
+        }
+        catch (NamingException e)
+        {
+            e.printStackTrace();
+        }
+        return null;}
 }
